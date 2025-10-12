7a829343414e63e4ca1f1075b4901fec0eadd00e
SeanAverS <seanasuguitan@gmail.com>
2025-10-12 02:35:14 -0700

    refactor: prompt hallucinates less on non guitar-related questions

--- backend/rag_service.py
+++ backend/rag_service.py
@@ -82,7 +82,7 @@ def rag_answer(user_query):
     prompt = (
         "You are a beginner guitar assistant. Only use guitar-related information. Do not answer unrelated questions."
         "The answer should be short but not too short. Be friendly and patient always!\n\n"
-        "If the context does not contain the answer, gracefully say you don't know about this question then only use your own knowledge to answer."
+        "If the context does not contain the answer, gracefully say you don't know about this question **then only use your own knowledge to answer.**"
         f"Context:\n{context}\n\n"
         f"Question:\n{user_query}\n\nAnswer:"
     )
